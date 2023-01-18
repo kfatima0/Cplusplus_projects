88cc61855539433d561b883283b29e3aff5f398f
kfatima0 <kanizfatima662@gmail.com>
2023-01-18 10:41:22 +0600

    First plotting in python

---
+++ first_project.py
@@ -0,0 +1,11 @@
+
+#matplotlib example   
+import matplotlib.pyplot as plt
+
+xpoints =[1,2,3,4,5,6,7,8,9,10]
+ypoints =[2,3,4,5,6,7,8,9,10,11]
+zpoints = xpoints*200
+
+
+plt.plot(xpoints, ypoints)
+plt.show()
